40f6e2863a77e9cd5fb35660d36719bd4f775e08
echo9z <1131223522@qq.com>
2022-04-24 17:19:16 +0800

    调整项目结构

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+// 加载全局样式
+import './style/index.less'
 
+Vue.config.productionTip = false // 关闭生产环境
+
+// 创建vue 根实例，将router store配置到根实例中
+// 通过render函数将 将app组件 渲染到 #app节点上
 new Vue({
   router,
   store,
